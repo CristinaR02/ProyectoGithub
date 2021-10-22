70219f8ca2e70b0296f16eee695509f03133e431
Nullgosling <Nullgosling@172.26.1.227>
2021-10-22 14:30:53 +0200

    Hola

---
+++ ProyectoPrueba2/src/nuevaPrueba.java
@@ -0,0 +1,13 @@
+
+public class nuevaPrueba {
+
+	public nuevaPrueba() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Esto es una prueba");
+	}
+
+}
